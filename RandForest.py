cf980bd27b990df3a2ebcba36617af781f2923da
DubyaDude <xxxshadowcraftxxx@gmail.com>
2018-09-15 07:31:33 -0400

    Cleaned Up

--- RandForest.py
+++ RandForest.py
@@ -1,21 +1,17 @@
 import pandas as pd
-import pytrends
-from pytrends.request import TrendReq
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import urllib
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 
-url = "https://api.iextrading.com/1.0/stock/AMD/chart/5y?filter=date,close"
-urlInfo = "https://api.iextrading.com/1.0/stock/AMD/company"
+stock = "AMD"
+url = "https://api.iextrading.com/1.0/stock/"+stock+"/chart/5y?filter=date,close"
+urlInfo = "https://api.iextrading.com/1.0/stock/"+stock+"/company"
 closingprices = []
 dates = []
 data = pd.DataFrame(pd.read_json(url, orient="columns"))
-# pd.set_option("display.max_rows", 105)
 for i in data:
     dates.append(data["date"])
     closingprices.append(data["close"])
@@ -32,12 +28,6 @@ with open("trends.txt") as f:
         int(score)
 
 
-
-# print("----------------------------INITIAL DATA------------------------------")
-# print(cp)
-# print(retTr)
-# print("------------------------GOOGLE TRENDS API ARRAY-----------------------")
-# print(ratings)
 print(len(scores))
 DATES = np.array(dates)
 CLOSINGPRICES = np.array(cp)
@@ -58,21 +48,7 @@ train_x = [tempDF["ClosingPrice"][:-52]]
 test_x = [tempDF["ClosingPrice"][-52:]]
 train_y = [tempDF["GoogleTrendsScore"][:-52]]
 test_y = [tempDF["GoogleTrendsScore"][-52:]]
-# lm.fit(train_x, train_y)
-# stock_y_pred = lm.predict(train_x)
-# train_x, test_x, train_y, test_y = train_test_split(tempDF.values, tempDF.target, test_size=0.2, random_state=0)
-# lm.fit(train_x, train_y)
-# lm.score(test_x, test_y)
-#
-# print(lm.coef_)
-# print(lm.intercept_)
-# y_pred = lm.predict(test_x)
-# plt.plot(test_y, y_pred, ".")
-# x = np.linspace(0, 100, 100)
-# y = x
-# plt.plot(x,y)
-# plt.show()
-# Plot data
+
 ## Original Graph
 plt.scatter(train_x, train_y, color="black")
 plt.ylabel("Stock Price")
