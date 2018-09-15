a7acd7cc126bca118d5465b743fec0e8f4652ff9
asarj <17691947+asarj@users.noreply.github.com>
2018-09-15 04:28:58 -0400

    added metrics for closing time

--- RandForest.py
+++ RandForest.py
@@ -3,13 +3,15 @@ import pytrends
 from pytrends.request import TrendReq
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import urllib
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 
-stock = "AMD"
-
-url = "https://api.iextrading.com/1.0/stock/" + stock + "/chart/5y?filter=date,close"
-urlInfo = "https://api.iextrading.com/1.0/stock/" + stock + "/company"
+url = "https://api.iextrading.com/1.0/stock/AMD/chart/5y?filter=date,close"
+urlInfo = "https://api.iextrading.com/1.0/stock/google/company"
 closingprices = []
 dates = []
 data = pd.DataFrame(pd.read_json(url, orient="columns"))
@@ -21,17 +23,34 @@ for i in data:
 cp = list(reversed(closingprices))
 
 pytrends = TrendReq(hl='en-US', tz=360)
-kw_list = [stock] 
+kw_list = ["google"]
 pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
 returnedTrend=pytrends.interest_over_time()
 retTr = returnedTrend.iloc[::-1]
-ratings = retTr[stock].tolist()
+ratings = retTr["google"].tolist()
 
 print("----------------------------INITIAL DATA------------------------------")
 print(cp)
 print(retTr)
 print("------------------------GOOGLE TRENDS API ARRAY-----------------------")
 print(ratings)
+print(len(ratings))
+DATES = np.array(dates)
+CLOSINGPRICES = np.array(cp)
+TRENDSCORES = np.array(ratings[::-1])
+DatesDF = pd.DataFrame(data=DATES)
+pricesDF = pd.DataFrame(data=CLOSINGPRICES)
+trendsDF = pd.DataFrame(data=TRENDSCORES.T)
+
+completeDF = pd.DataFrame()
+completeDF = completeDF.assign(Date=DATES[0])
+completeDF = completeDF.assign(ClosingPrice=CLOSINGPRICES[0])
+completeDF = completeDF.assign(GoogleTrendsScore=trendsDF[0])
+print(completeDF.loc[0:260])
+
+
+
+
 
 
 
