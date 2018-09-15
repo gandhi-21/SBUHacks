84f1dc51067c970ee9b8953f6afee232c7bb2362
asarj <17691947+asarj@users.noreply.github.com>
2018-09-15 05:18:23 -0400

    started linear regression model

--- RandForest.py
+++ RandForest.py
@@ -5,13 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import urllib
-from sklearn import datasets, metrics
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 
 url = "https://api.iextrading.com/1.0/stock/AMD/chart/5y?filter=date,close"
-urlInfo = "https://api.iextrading.com/1.0/stock/google/company"
+urlInfo = "https://api.iextrading.com/1.0/stock/AMD/company"
 closingprices = []
 dates = []
 data = pd.DataFrame(pd.read_json(url, orient="columns"))
@@ -23,30 +23,43 @@ for i in data:
 cp = list(reversed(closingprices))
 
 pytrends = TrendReq(hl='en-US', tz=360)
-kw_list = ["google"]
+kw_list = ["AMD"]
 pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
 returnedTrend=pytrends.interest_over_time()
 retTr = returnedTrend.iloc[::-1]
-ratings = retTr["google"].tolist()
+ratings = retTr["AMD"].tolist()
 
-print("----------------------------INITIAL DATA------------------------------")
-print(cp)
-print(retTr)
-print("------------------------GOOGLE TRENDS API ARRAY-----------------------")
-print(ratings)
+# print("----------------------------INITIAL DATA------------------------------")
+# print(cp)
+# print(retTr)
+# print("------------------------GOOGLE TRENDS API ARRAY-----------------------")
+# print(ratings)
 print(len(ratings))
 DATES = np.array(dates)
 CLOSINGPRICES = np.array(cp)
-TRENDSCORES = np.array(ratings[::-1])
-DatesDF = pd.DataFrame(data=DATES)
-pricesDF = pd.DataFrame(data=CLOSINGPRICES)
+TRENDSCORES = np.array(ratings)
+DatesDF = pd.DataFrame(data=DATES.T)
+pricesDF = pd.DataFrame(data=CLOSINGPRICES.T)
 trendsDF = pd.DataFrame(data=TRENDSCORES.T)
 
 completeDF = pd.DataFrame()
 completeDF = completeDF.assign(Date=DATES[0])
 completeDF = completeDF.assign(ClosingPrice=CLOSINGPRICES[0])
 completeDF = completeDF.assign(GoogleTrendsScore=trendsDF[0])
-print(completeDF.loc[0:260])
+print(completeDF)
+##### THIS IS TEMPORARY, GOOGLE TRENDS API ONLY RETRIEVES 260 RECENT QUERIES
+tempDF = completeDF.loc[0:260]
+lm = LinearRegression()
+train_x = tempDF["ClosingPrice"][:-52]
+test_x = tempDF["ClosingPrice"][-52:]
+train_y = tempDF["GoogleTrendsScore"][:-52]
+test_y = tempDF["GoogleTrendsScore"][-52:]
+
+lm.fit(train_x, train_y)
+stock_y_pred = lm.predict(train_x)
+
+
+#####
 
 
 
