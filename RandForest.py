267d8326243aba7535fff0adb6a021b827392c42
DubyaDude <xxxshadowcraftxxx@gmail.com>
2018-09-15 07:36:39 -0400

    CleanUp+Comments

--- RandForest.py
+++ RandForest.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-
+#GRABS DATA FROM STOCK API AND PUTS IT IN DATAFRAME
 stock = "AMD"
 url = "https://api.iextrading.com/1.0/stock/"+stock+"/chart/5y?filter=date,close"
 urlInfo = "https://api.iextrading.com/1.0/stock/"+stock+"/company"
@@ -16,7 +16,9 @@ for i in data:
     dates.append(data["date"])
     closingprices.append(data["close"])
 
+#REVERSES SO THE LATEST IS ON TOP OF LIST
 cp = list(reversed(closingprices))
+
 
 scores = []
 with open("trends.txt") as f:
@@ -41,6 +43,7 @@ completeDF = completeDF.assign(Date=DATES[0])
 completeDF = completeDF.assign(ClosingPrice=CLOSINGPRICES[0])
 completeDF = completeDF.assign(GoogleTrendsScore=trendsDF[0])
 print(completeDF)
+
 ##### THIS IS TEMPORARY, GOOGLE TRENDS API ONLY RETRIEVES 260 RECENT QUERIES
 tempDF = completeDF.loc[0:260]
 lm = LinearRegression()
@@ -68,11 +71,3 @@ plt.plot(np.unique(tempDF["ClosingPrice"]), np.poly1d(np.polyfit(tempDF["Closing
 plt.ylabel("Stock Price")
 plt.xlabel("Google Trend Score")
 plt.show()
-# plt.scatter()
-
-
-
-
-# print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-# print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-# print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
